60de3da622ac818b441f371cc47fdff7f55ef753
Zaid_ZiD <zid6177@gmail.com>
2023-05-19 22:04:59 +0500

    test div added in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
 
 const App = () => {
   return (
     <div>
       <h1>
-        E-Commerce
+        E-Commerce app
+
+        <br/>
+
+        <div style={{border:"2px solid black"}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure atque deleniti molestias officia omnis totam nulla obcaecati labore rem error, officiis eius voluptatem nemo recusandae ipsa modi, eaque cumque natus.
+        </div>
+
       </h1>
     </div>
   )
